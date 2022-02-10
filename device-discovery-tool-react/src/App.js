50aa08f64e31b235464d18358a5685c36cfea115
DJKonnect <seregahop@yahoo.com>
2022-02-10 10:32:45 -0800

    Updated styling on Login Page, Using Flexbox

--- device-discovery-tool-react/src/App.js
+++ device-discovery-tool-react/src/App.js
@@ -1,5 +1,5 @@
 import {Container} from 'react-bootstrap'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -11,10 +11,10 @@ function App() {
     <Router>
       <Header />
       <main className="py-3">
-        <Container>
+        <Switch>
           <Route path='/' component={HomeScreen} exact />
           <Route path="/auth" component={auth}/>
-        </Container>
+        </Switch>
       </main>
       <Footer />
     </Router>
--- device-discovery-tool-react/src/components/auth.js
+++ device-discovery-tool-react/src/components/auth.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import logo from './logo.png';
-import '../App.css';
+import React, { Component } from "react"
+import logo from './logo.png'
+import '../App.css'
 
 
-function Auth()
-{
-    return (
-        
-        <form className="authForm">
-        <div class="login-image">
-            <img src={logo} />
-        </div>
-        <div class="login-fields">
-            <label for="exampleInputEmail1">Email Address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-            <br></br>
-        </div>
-        <div class="login-fields">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-        </div>
-        <div class="login-fields form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Remember Me</label>
-        </div>
-        <div class="button">
-            <button type="submit" class="btn btn-primary">Log In</button>
-        </div>
-        </form>
-    )
+
+export default class Auth extends Component {
+    render() {
+        return (
+            <div className="auth-wrapper">
+              <div className="auth-inner">
+                <form>
+                <div class="login-image">
+                    <img src={logo} />
+                </div>
+                <h3>Sign In</h3>
+                <div class="login-fields">
+                    <label for="exampleInputEmail1">Email Address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <br></br>
+                </div>
+                <div class="login-fields">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                </div>
+                <div class="login-fields form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                </div>
+                <div class="button">
+                    <button type="submit" class="btn btn-primary">Log In</button>
+                </div>
+                </form>
+            </div>
+          </div>
+        );
+    }
 }
-
-export default Auth;
