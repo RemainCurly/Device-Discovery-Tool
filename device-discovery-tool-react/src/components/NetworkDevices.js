846f49db1763f2d242d80ff90c8d3c5796e3789b
Samwich422 <samanthatrevino@csus.edu>
2022-03-15 19:58:14 -0700

    TA-127
    network devices table is now scrollable

--- device-discovery-tool-react/src/components/NetworkDevices.js
+++ device-discovery-tool-react/src/components/NetworkDevices.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Table } from 'react-bootstrap';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as ReactBootStrap from "react-bootstrap";
 
 function NetworkDevices(props) {
 
@@ -11,34 +12,36 @@ function NetworkDevices(props) {
     return (
         <div className="main">
             <h1><center>Network Devices</center></h1>
-            <Table striped bordered hover>
-            <thead>
-                     <tr>
-                        <th>Favorite</th>
-                        <th>Device Name</th>
-                        <th>IP Address</th>
-                        <th>MAC Address</th>
-                        <th>Description</th>
-                        <th>Status</th>
-                        <th>Location</th>
-                    </tr>
-             </thead>
-                {/* Only show devices that exist in the DB */}
-                { props.devices && props.devices.map( device => {
-                    return (
-                        <tbody key={device.id}>
-                                <tr>
-                                    <td><Form.Check onClick={() => toggleFavorite(device)} /></td>
-                                    <td>{device.name}</td>
-                                    <td>{device.ip}</td>
-                                    <td>{device.mac}</td>
-                                    <td>{device.description}</td>
-                                    <td>{device.isUp}</td>
-                                </tr>
-                        </tbody>
-                    )
-                })}
-        </Table>
+            <div class="scrollable">
+                <ReactBootStrap.Table striped bordered hover>
+                <thead>
+                        <tr> 
+                            <th>Favorite</th>
+                            <th>Device Name</th>
+                            <th>IP Address</th>
+                            <th>MAC Address</th>
+                            <th>Description</th>
+                            <th>Status</th>
+                            <th>Location</th>
+                        </tr>
+                </thead>
+                    {/* Only show devices that exist in the DB */}
+                    { props.devices && props.devices.map( device => {
+                        return (
+                            <tbody key={device.id}>
+                                    <tr>
+                                        <td><Form.Check onClick={() => toggleFavorite(device)} /></td>
+                                        <td>{device.name}</td>
+                                        <td>{device.ip}</td>
+                                        <td>{device.mac}</td>
+                                        <td>{device.description}</td>
+                                        <td>{device.isUp}</td>
+                                    </tr>
+                            </tbody>
+                        )
+                    })}
+            </ReactBootStrap.Table>
+            </div>
         </div>
     )
 }
