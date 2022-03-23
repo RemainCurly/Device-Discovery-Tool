5123925f93323eb417cdd0055626d551779d0334
RemainCurly <63758571+RemainCurly@users.noreply.github.com>
2022-03-23 09:45:26 -0700

    TA-140
    Imported CanvasJS and begun working on the graph component

--- device-discovery-tool-react/src/App.js
+++ device-discovery-tool-react/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './components/HomeScreen'
