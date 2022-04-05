398741451fbe1db4bfe26a18a15fbe77ac679224
hkhosa25 <hkhosa@csus.edu>
2022-04-04 21:25:31 -0700

    TA-143
    Made table headers for returned device information

--- device-discovery-tool-react/src/components/DeviceUtility.js
+++ device-discovery-tool-react/src/components/DeviceUtility.js
@@ -3,16 +3,13 @@ import { Button } from 'react-bootstrap';
 import '../App.css';
 import { Row, Col } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap'
+import { Table } from 'react-bootstrap';
 
 function DeviceUtility(props) {
 
-    const toggleFavorite = device => {
-        props.toggleFavorite(device);
-    }
-
-
     return (
         <div>
+            <h1><center>Device Utilities</center></h1>
             <Row>
                 <Col>
                     <div className='row m-3'>
@@ -24,10 +21,37 @@ function DeviceUtility(props) {
                     </div>
                 </Col>
                 <Col>
-                    <h1>This page shows the device utilities.</h1>
+                    <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>Device Name</th>
+                                    <th>Device Type</th>
+                                    <th>CPU</th>
+                                    <th>RAM</th>
+                                    <th>Disk</th>
+                                    <th>Network</th>
+                                    <th>MAC Address</th>
+                                </tr>
+                            </thead>
+                            {/* Only show devices that exist in the DB */}
+                            {props.devices && props.devices.map(device => {
+                                return (
+                                    <tbody key={device.id}>
+                                            <tr>
+                                                <td>{device.name}</td>
+                                                <td>{device.type}</td>
+                                                <td>{device.cpu}</td>
+                                                <td>{device.ram}</td>
+                                                <td>{device.disk}</td>
+                                                <td>{device.network}</td>
+                                                <td>{device.mac}</td>
+                                            </tr>
+                                    </tbody>
+                                )
+                            })}
+                    </Table>   
                 </Col>
-                <Col>
-                </Col>
+                <Col></Col>
             </Row>
         </div>
     )
