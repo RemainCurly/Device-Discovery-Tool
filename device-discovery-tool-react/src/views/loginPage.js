330b01a790d91b4ff9c0baadae697fabc83fa9b1
RemainCurly <63758571+RemainCurly@users.noreply.github.com>
2022-05-03 23:02:05 -0700

    TA-224
    Made some visual adjustments regarding the Login page

--- device-discovery-tool-react/src/views/loginPage.js
+++ device-discovery-tool-react/src/views/loginPage.js
@@ -16,27 +16,27 @@ const LoginPage = () => {
   return (
     <section>
       <form onSubmit={handleSubmit}>
-      <div className="auth-wrapper">
-              <div className="auth-inner">
-                  <div className="login-image">
-                      <img src={logo} alt="STAPL Logo"/>
-                  </div>
-                  <h3>Sign In</h3>
-                  <div className="login-fields">
-                      <label htmlFor="username">Username</label>
-                      <input type="text" id="username" placeholder="Enter Username" required/>
-                      <br></br>
-                  </div>
-                  <div className="login-fields">
-                      <label htmlFor="password">Password</label>
-                      <input type="password" id="password" placeholder="Enter Password" required />
-                  </div>
-                  <div className="button">
-                      <button type="submit" className="btn btn-primary">Log In</button>
-                  </div>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <div className="login-image">
+              <img src={logo} alt="STAPL Logo" className="login-image" />
+            </div>
+            <h3>Sign In</h3>
+            <div className="login-fields">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" placeholder="Enter Username" required />
+              <br></br>
+            </div>
+            <div className="login-fields">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" placeholder="Enter Password" required />
+            </div>
+            <div className="button">
+              <button type="submit" className="btn btn-primary">Log In</button>
             </div>
           </div>
-          </form>
+        </div>
+      </form>
     </section>
   );
 };
